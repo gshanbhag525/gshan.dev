be759da3ef141456b2be43fd9384c2d382d5b6b6
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-10-14 20:58:56 +0530

    fixed desc not appearing in myworks mobile layout and cleanup

--- src/components/myVideos/myVideos.jsx
+++ src/components/myVideos/myVideos.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import "./myVideos.scss";
 
 export default function MyVideos() {
-  const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
       id: "1",
@@ -41,17 +39,11 @@ export default function MyVideos() {
     },
   ];
 
-  const handleClick = (way) => {
-    way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
-      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
-  };
   
   return (
     <div className="videos" id="myvideos">
       <div
         className="slider"
-        // style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
         <h1> My Videos </h1>
         {data.map((d) => (
@@ -67,9 +59,7 @@ export default function MyVideos() {
               </div>
               <div className="left">
                 <div className="leftContainer">
-                  {/* <div className="imgContainer">
-                    <img src={d.icon} alt="" />
-                  </div> */}
+                  
                   <h2>{d.title}</h2>
                   <span>YouTube - ProgrammingKnowledge</span>
                   <p>{d.desc}</p>
@@ -81,18 +71,7 @@ export default function MyVideos() {
           </div>
         ))}
       </div>
-      {/* <img
-        src="assets/arrow.png"
-        className="arrow left"
-        alt=""
-        onClick={() => handleClick("left")}
-      />
-      <img
-        src="assets/arrow.png"
-        className="arrow right"
-        alt=""
-        onClick={() => handleClick()}
-      /> */}
+      
     </div>
   );
 }
--- src/components/myWorks/myWorks.jsx
+++ src/components/myWorks/myWorks.jsx
@@ -1,88 +1,58 @@
-import { useState } from "react";
 import "./myWorks.scss";
 
 export default function MyWorks() {
-  const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
       id: "1",
       icon: "./assets/mobile.png",
       title: "SnapOrder Consumer App",
-      desc:
-        "Incorporated JWT based authentication system.",
-      img:
-        "./assets/snaporder.png",
+      desc: "Incorporated JWT based authentication system.",
+      img: "./assets/snaporder.png",
+      url: "https://play.google.com/store/apps/details?id=com.snapbizz.snaporder&hl=en_IN&gl=US",
     },
     {
       id: "2",
       icon: "./assets/mobile.png",
       title: "SnapOrder Merchant App",
-      desc:
-        "Implemented product search used by 10k+ kirana stores.",
-      img:
-        "./assets/snapmerchant.png",
+      desc: "Implemented product search used by 10k+ kirana stores.",
+      img: "./assets/snapmerchant.png",
+      url: "https://play.google.com/store/apps/details?id=com.snapbizz.snapmerchant&hl=en_IN&gl=US",
     },
     {
       id: "3",
       icon: "./assets/mobile.png",
       title: "Fiserv App",
-      desc:
-        "Implemented product search used by 10k+ kirana stores.",
-      img:
-      "./assets/fiserv.png",
+      desc: "Implemented product search used by 10k+ kirana stores.",
+      img: "./assets/fiserv.png",
+      url: "",
     },
   ];
 
-  const handleClick = (way) => {
-    way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
-      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
-  };
-  
   return (
-    <div className="works" id="myworks">
-      <div
-        className="slider"
-        // style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
-      >
+    <div className="myworks" id="myworks">
+      <div className="slider">
         <h1> My Works </h1>
         {data.map((d) => (
           <div className="container">
             <div className="item">
               <div className="right">
-                <img
-                  src={d.img}
-                  alt=""
-                />
+                <a href={d.url} target="_blank">
+                  <img src={d.img} alt="" />
+                </a>
               </div>
               <div className="left">
                 <div className="leftContainer">
-                  {/* <div className="imgContainer">
-                    <img src={d.icon} alt="" />
-                  </div> */}
                   <h2>{d.title}</h2>
-                  <span>SDE - SnapBizz CloudTech Pvt. Ltd.</span>
-                  <p>{d.desc}</p>
-                  
-                </div>
 
+                  <span>SDE - SnapBizz CloudTech Pvt. Ltd.</span>
+
+                  <p>{d.desc}</p>
+                </div>
               </div>
             </div>
           </div>
         ))}
       </div>
-      {/* <img
-        src="assets/arrow.png"
-        className="arrow left"
-        alt=""
-        onClick={() => handleClick("left")}
-      />
-      <img
-        src="assets/arrow.png"
-        className="arrow right"
-        alt=""
-        onClick={() => handleClick()}
-      /> */}
     </div>
   );
 }
